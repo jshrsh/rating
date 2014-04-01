db4eb86bb0853cc7ca0f472c9fccc477df7e584a
skywalker <satanavenger@gmail.com>
2014-04-02 00:01:07 +0600

    Create form.js

---
+++ form.js
@@ -0,0 +1,26 @@
+function submitForm() {
+        $("#customError").hide(); //clear old validation messages
+	$("#fm1").validate({
+		rules : {
+			'username' : {
+				required : true,
+				email  :true
+			},
+			'password' : 'required',
+			'userAccessAgreeKey' : {
+				agreementCheck : true
+			}
+		},
+		errorElement : 'span',
+		messages : {
+			'username' : {
+				required : 'Please enter a valid e-mail.',
+				email : 'Please enter a valid e-mail.'
+			},
+			'password' : 'Your password was entered incorrectly.',
+			'userAccessAgreeKey' : {
+				agreementCheck : 'You must agree with the Terms and Conditions to sign in.'
+			}
+		}
+	
+	});
