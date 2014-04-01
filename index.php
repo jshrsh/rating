fc100b3593aa8fb615f55ddbc77fc4b8562abcb4
ali6ka08 <ali6ka08@users.noreply.github.com>
2014-04-01 22:04:57 +0600

    Update index.php

--- index.php
+++ index.php
@@ -117,7 +117,18 @@
                 <!-- <li><a href="#" class="last">Contact</a></li> -->                     
             </ul> 
 	    </div> <!-- end of menu -->
-        
+        <?php 
+				if(isset($id) and !empty($id))
+				{
+					switch($id)
+					{
+						case 3:
+							include "Allprof.php";
+							exit;
+						break;
+					}
+				}
+			?>
         <div id="templatemo_banner">
         	
         </div> <!-- end of banner -->
