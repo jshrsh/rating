2a9716611081942fd6944ac7d5054b98f98871e9
skywalker <satanavenger@gmail.com>
2014-04-01 23:55:23 +0600

    Create reg.js

---
+++ reg.js
@@ -0,0 +1,22 @@
+<!--<div id="ad728">
+				
+				
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Ads - 728 x 90</title>
+
+</head>
+<body bgcolor="#fff">
+<script language="Javascript">
+	var ordJS=Math.floor(Math.random()*10000000000000000000);
+ 	var fnm = "";
+	var lp=location.pathname;
+	var sectionParamAndValue = "";
+	var sectionParam="";
+	var tempseg1="";
+	var sname1="";
+	var state1="";
+	var country1="null";
+	var iii=0;
+	var jjj=0;
